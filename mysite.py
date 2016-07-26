64c6815653e0437c4d63616252c74477712bad8f
Andrew Lubawy <dlubawy@gmail.com>
2016-07-25 19:42:03 -0700

    Finalised contact form

--- mysite.py
+++ mysite.py
@@ -1,14 +1,11 @@
-import os, smtplib
+import os, smtplib, urllib.request, urllib.parse, json
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from email.mime.text import MIMEText
 
 # create our little application :)
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(__name__)
-
-app.config.update(dict(
-    SECRET_KEY='dc3919eb32e433499beefaeb7509297542faa2729c81bb4c89e14699ee759995'
-    ))
+app.config.from_pyfile('development.cfg', silent=True)
 
 @app.route('/')
 def index():
@@ -23,20 +20,32 @@ def contact():
             flash("No name.", "warning")
         elif request.form['email'] == "":
             flash("No email.", "warning")
+        elif check_captcha(request.form['g-recaptcha-response']) == False:
+            flash("Captcha not verified", "danger")
         else:
             send_msg(request.form['name'], request.form['email'], request.form['message'])
             flash("Message sent.", "success")
             return redirect("http://127.0.0.1:5000/#contact")
     return redirect('http://127.0.0.1:5000/#contact')
 
-def send_msg(name, email, message):
-    msg = MIMEText(message)
-    msg['From'] = email
-    msg['To']  = "dlubawy@gmail.com"
-    msg['Subject'] = name
+def check_captcha(response):
+    data = {
+            "secret":app.config['CAPTCHA_SECRET'],
+            "response":response
+            }
+    url_data = urllib.parse.urlencode(data)
+    url = "https://www.google.com/recaptcha/api/siteverify?" + url_data
+    res = urllib.request.urlopen(url)
+    read = json.loads(res.read().decode('ascii'))
+    if read["success"] == True:
+        return True
+    return False
 
-    # The actual mail send
-    server = smtplib.SMTP('smtp-relay.gmail.com:25')
-    server.starttls()
-    server.ehlo(email)
-    server.quit()  
+def send_msg(name, email, message):
+    msg = name+"\n"+email+"\n"+message
+    data = {
+            "bot_id":app.config['BOT_ID'],
+            "text":msg
+            }
+    req = urllib.request.Request("https://api.groupme.com/v3/bots/post", data=json.dumps(data).encode("ascii"), headers={"Content-Type":"application/json"})
+    response = urllib.request.urlopen(req)
